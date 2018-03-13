f16df74e190ce17500ac29415746e96197ac912b
Panciu-Rusu Mihai <panciu.rusu.mihai@gmail.com>
2018-03-13 09:15:50 +0200

    Clasa postare

---
+++ facebook_IP/src/facebook_ip/Postare.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facebook_ip;
+import java.util.Vector;
+
+/**
+ *
+ * @author Mihai
+ */
+public class Postare {
+    int numarAprecieri;
+    int numarComentarii;
+    int numarDistribuiri;
+    Vector<Comentariu> comentarii;
+    
+    String continut; 
+    
+    public Postare()
+    {
+        numarAprecieri=0;
+        numarComentarii=0;
+        numarDistribuiri=0;
+    
+        continut=""; 
+        comentarii= new Comentariu<>();
+    }
+    
+    
+    public void apriecierePostare()
+    {
+        ++numarAprecieri;
+    }
+    
+    public void adaugaComentariu()
+    {
+        
+    }
+}
